d2f19509b1272ec3203a786b6127d7201a09a71f
spon <alexandre.lanvin@etu.umontpellier.fr>
2022-01-30 16:26:04 +0100

    TP1 done

--- TP1_code/TP1/TP1.cpp
+++ TP1_code/TP1/TP1.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <iostream>
+#include <time.h>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -42,6 +43,9 @@ float lastFrame = 0.0f;
 float angle = 0.;
 float zoom = 1.;
 
+//SCENES (false: 3 chaises ou true: le plan texturé)
+bool scene_1 = false;
+
 void generateVertices(std::vector<glm::vec3> &indexed_vertices, std::vector<glm::vec2> &uv, int w, int h, vec3 pos){
     float current_x, current_y;
     int res_x = 16;       //16x16 vertices
@@ -72,6 +76,15 @@ void generateVertices(std::vector<glm::vec3> &indexed_vertices, std::vector<glm:
     }
 }
 
+void randomizeHeight(std::vector<glm::vec3> &indexed_vertices, int bound) {
+    srand(time(NULL));
+    for(int i = 0; i < indexed_vertices.size();i ++){
+        indexed_vertices[i][2] =  1. / (float) (1 + std::rand() % bound); 
+        // std::cout<<vert[2]<<std::endl;
+        std::cout<< 1. / (float) (0.3 + std::rand() % bound)<<std::endl;
+    }
+}
+
 void generatePlan(std::vector<std::vector<short unsigned int>> &triangles, std::vector<glm::vec3> &indexed_vertices, std::vector<glm::vec2> &uv, int w, int h, vec3 pos) {
     int res_x = 16;       //16x16 vertices
     int res_y = 16;       //16x16 vertices
@@ -80,6 +93,7 @@ void generatePlan(std::vector<std::vector<short unsigned int>> &triangles, std::
     int tr_index = 0;
 
     generateVertices(indexed_vertices, uv, w, h, pos);
+
     
     for(unsigned short i = 0; i < res_x - 1; i ++){
         for(unsigned short j = 0; j < res_y - 1; j ++) {
@@ -170,9 +184,12 @@ int main( void )
     // Create and compile our GLSL program from the shaders
     GLuint programID = LoadShaders( "vertex_shader.glsl", "fragment_shader.glsl" );
 
-    /*****************TODO***********************/
     // Get a handle for our "Model View Projection" matrices uniforms
     GLuint mvp_handle = glGetUniformLocation(programID, "mvp");
+
+    //UV buffer
+    GLuint uvbuffer;
+
 
     /****************************************/
     std::vector<unsigned short> indices; //Triangles concaténés dans une liste
@@ -181,20 +198,40 @@ int main( void )
 
     //Chargement du fichier de maillage
     std::string filename("chair.off");
-    loadOFF(filename, indexed_vertices, indices, triangles );
+    if(scene_1){
+        loadOFF(filename, indexed_vertices, indices, triangles );
 
-    GLuint texture = loadBMP_custom("../tex.bmp");
-    // // Load it into a VBO
-    // GLuint vertexbuffer;
-    // glGenBuffers(1, &vertexbuffer);
-    // glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // glBufferData(GL_ARRAY_BUFFER, indexed_vertices.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
+    }else {
 
-    // // Generate a buffer for the indices as well
-    // GLuint elementbuffer;
-    // glGenBuffers(1, &elementbuffer);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0] , GL_STATIC_DRAW);
+        GLuint texture = loadBMP_custom("../tex.bmp");
+        std::vector<glm::vec2> vert_uv;
+        generatePlan(triangles, indexed_vertices, vert_uv, 4, 4, vec3(0, 0, 0));
+        randomizeHeight(indexed_vertices, 10);
+
+        indices.resize(3 * triangles.size());
+
+        // for(vec3 v : indexed_vertices)
+        //     std::cout<<v[0]<<", "<<v[1]<<", "<<v[2]<<std::endl;
+
+        
+        int k = 0;
+        for(int i = 0; i < triangles.size(); i ++){
+            indices[k] = triangles[i][0]; 
+            indices[k + 1] = triangles[i][1]; 
+            indices[k + 2] = triangles[i][2]; 
+            k += 3;
+        }
+
+        // Generate a buffer for UVs
+        glGenBuffers(1, &uvbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glBufferData(GL_ARRAY_BUFFER, vert_uv.size() * sizeof(glm::vec2), &vert_uv[0], GL_STATIC_DRAW);
+
+        // for(vec2 uv : vert_uv){
+        //     std::cout<<uv[0]<<", "<<uv[1]<<std::endl;
+        // }
+    }
+
 
     // Get a handle for our "LightPosition" uniform
     glUseProgram(programID);
@@ -207,46 +244,25 @@ int main( void )
     int nbFrames = 0;
 
     //PLANE....
-    std::vector<std::vector<unsigned short> > triangles_arr;
-    std::vector<glm::vec3> indexed_vert;
-    std::vector<glm::vec2> vert_uv;
-    std::vector<unsigned short> soup;
-
-    generatePlan(triangles_arr, indexed_vert, vert_uv, 4, 4, vec3(0, 0, 0));
-
-    soup.resize(3 * triangles_arr.size());
-    int k = 0;
-    for(int i = 0; i < triangles_arr.size(); i ++){
-        soup[k] = triangles_arr[i][0]; 
-        soup[k + 1] = triangles_arr[i][1]; 
-        soup[k + 2] = triangles_arr[i][2]; 
-        k += 3;
-    }
-
-    for(vec2 uv : vert_uv){
-        std::cout<<uv[0]<<", "<<uv[1]<<std::endl;
-    }
+    // std::vector<std::vector<unsigned short> > triangles_arr;
+    // std::vector<glm::vec3> indexed_vert;
+    // std::vector<unsigned short> soup;
 
     // Load it into a VBO
     GLuint vertexbuffer;
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, indexed_vert.size() * sizeof(glm::vec3), &indexed_vert[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, indexed_vertices.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
 
     // Generate a buffer for the indices as well
     GLuint elementbuffer;
     glGenBuffers(1, &elementbuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, soup.size() * sizeof(unsigned short), &soup[0] , GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0] , GL_STATIC_DRAW);
 
-    // Generate a buffer for UVs
-    GLuint uvbuffer;
-    glGenBuffers(1, &uvbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    glBufferData(GL_ARRAY_BUFFER, vert_uv.size() * sizeof(glm::vec2), &vert_uv[0], GL_STATIC_DRAW);
 
-    std::cout<<triangles_arr.size()<<std::endl;
-    std::cout<<soup.size()<<std::endl;
+    std::cout<<triangles.size()<<std::endl;
+    std::cout<<indices.size()<<std::endl;
 
     do{
 
@@ -284,6 +300,12 @@ int main( void )
         // in the "Model View Projection" to the shader uniforms
         Model =  glm::translate(Model, vec3(-0.5, 0, 0));
 
+        if(!scene_1){
+            Model = glm::translate(Model, glm::vec3(2, 0, 0.));
+            Model = glm::rotate(Model, angle, glm::vec3(0, 1, 1));
+            Model = glm::translate(Model, glm::vec3(-1, 0, 0.));
+        }
+
         glm::mat4 mvp = Projection * View * Model;
         glUniformMatrix4fv(mvp_handle, 1, GL_FALSE, &mvp[0][0]);
         
@@ -305,17 +327,20 @@ int main( void )
                     (void*)0            // array buffer offset
                     );
 
-        // UVs buffer                   // only for PLANE
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-        glVertexAttribPointer(
-                1,                  // attribute
-                2,                  // size
-                GL_FLOAT,           // type
-                GL_FALSE,           // normalized?
-                0,                  // stride
-                (void*)0            // array buffer offset
-        );
+        if(!scene_1){
+
+            // UVs buffer                   // only for PLANE
+            glEnableVertexAttribArray(1);
+            glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+            glVertexAttribPointer(
+                    1,                  // attribute
+                    2,                  // size
+                    GL_FLOAT,           // type
+                    GL_FALSE,           // normalized?
+                    0,                  // stride
+                    (void*)0            // array buffer offset
+            );
+        }
 
         // Index buffer
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
@@ -323,37 +348,40 @@ int main( void )
         // Draw the triangles !
         glDrawElements(
                     GL_TRIANGLES,      // mode
-                    soup.size(),    // count            // SOUP for plane, INDICES for chairs
+                    indices.size(),    // count            // SOUP for plane, INDICES for chairs
                     GL_UNSIGNED_SHORT,   // type
                     (void*)0           // element array buffer offset
                     );
+        
+        if(scene_1){
 
-        // //2nd chair
-        // glm::mat4 Model2 =  glm::rotate(Model, glm::radians(180.0f), glm::vec3(0, 1, 0));
-        // Model2 = glm::translate(Model2, glm::vec3(-1, 0, 0));
-        // mvp = Projection * View * Model2;
-        // glUniformMatrix4fv(mvp_handle, 1, GL_FALSE, &mvp[0][0]);
-        // glDrawElements(
-        //             GL_TRIANGLES,      // mode
-        //             indices.size(),    // count
-        //             GL_UNSIGNED_SHORT,   // type
-        //             (void*)0           // element array buffer offset
-        //             );
+            //2nd chair
+            glm::mat4 Model2 =  glm::rotate(Model, glm::radians(180.0f), glm::vec3(0, 1, 0));
+            Model2 = glm::translate(Model2, glm::vec3(-1, 0, 0));
+            mvp = Projection * View * Model2;
+            glUniformMatrix4fv(mvp_handle, 1, GL_FALSE, &mvp[0][0]);
+            glDrawElements(
+                        GL_TRIANGLES,      // mode
+                        indices.size(),    // count
+                        GL_UNSIGNED_SHORT,   // type
+                        (void*)0           // element array buffer offset
+                        );
 
-        // //3rd chair
-        // glm::mat4 Model3 = glm::mat4(1.0f);
-        // Model3 = glm::translate(Model3, glm::vec3(0, 1, 0.));
-        // Model3 = glm::rotate(Model3, angle, glm::vec3(0, 0, 1));
-        // Model3 = glm::translate(Model3, glm::vec3(0, -0.5, 0.));
+            //3rd chair
+            glm::mat4 Model3 = glm::mat4(1.0f);
+            Model3 = glm::translate(Model3, glm::vec3(0, 1, 0.));
+            Model3 = glm::rotate(Model3, angle, glm::vec3(0, 0, 1));
+            Model3 = glm::translate(Model3, glm::vec3(0, -0.5, 0.));
 
-        // mvp = Projection * View * Model3;
-        // glUniformMatrix4fv(mvp_handle, 1, GL_FALSE, &mvp[0][0]);
-        // glDrawElements(
-        //             GL_TRIANGLES,      // mode
-        //             indices.size(),    // count
-        //             GL_UNSIGNED_SHORT,   // type
-        //             (void*)0           // element array buffer offset
-        //             );
+            mvp = Projection * View * Model3;
+            glUniformMatrix4fv(mvp_handle, 1, GL_FALSE, &mvp[0][0]);
+            glDrawElements(
+                        GL_TRIANGLES,      // mode
+                        indices.size(),    // count
+                        GL_UNSIGNED_SHORT,   // type
+                        (void*)0           // element array buffer offset
+                        );
+        }
 
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
@@ -415,6 +443,7 @@ void processInput(GLFWwindow *window)
     //Big chair rotation
     if(glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
         angle += cameraSpeed;
+
     
 }
 
---
+++ TP1_code/readme.md
@@ -0,0 +1,31 @@
+# TP1 Moteur de Jeux
+
+## Usage
+
+### Project setup
+
+> ./reset_dir.sh (supprime l'ancien dossier build et le reconstruit)
+>
+> cd build; make -j
+>
+> ./launch-TP1.sh
+
+### Raccourcis
+
+> Z, S : zoom/unzom.
+
+> &#8592;, &#8593;, &#8594;, &#8595; :  translation de la caméra.
+
+> R : rotation de la 3 ème chaise ou du plan texturé.
+
+>**ligne 47 TP1.cpp** ``bool scene_1 = false;
+>`` :  modifier à **true** pour afficher le plan texturé.
+
+## Aperçu des scènes
+
+| <img src="./screens/Chairs_1.png" style="zoom:33%;"/> | <img src="./screens/plane_1.png" style="zoom:33%;" /> |
+| ----------------------------------------------------- | ----------------------------------------------------- |
+| <img src="./screens/plane_2.png" style="zoom:33%;" /> | <img src="./screens/plane_3.png" style="zoom:33%;" /> |
+
+
+
